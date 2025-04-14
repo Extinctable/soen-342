27e9a32636638ce8d9a7d47167421369759be163
Extinctable <massimo02caruso@gmail.com>
2025-04-14 00:22:50 -0400

    Views

---
+++ src/Iteration3/src/view/AdminView.java
@@ -0,0 +1,25 @@
+package view;
+
+public class AdminView {
+
+    public void displayMenu() {
+        System.out.println("=== Admin Menu ===");
+        System.out.println("1. Add Auction House");
+        System.out.println("2. Add Auction");
+        System.out.println("3. Add Art Object");
+        System.out.println("4. Add Expert");
+        System.out.println("5. Approve Clients");
+        System.out.println("6. View Auctions");
+        System.out.println("7. View Art Objects");
+        System.out.println("8. View Experts");
+        System.out.println("9. View Clients");
+        System.out.println("10. View Service Requests");
+        System.out.println("11. View Auction Houses");
+        System.out.println("0. Logout");
+        System.out.print("Select an option: ");
+    }
+    
+    public void showSuccessMessage(String message) {
+        System.out.println("[Admin] " + message);
+    }
+}
---
+++ src/Iteration3/src/view/ClientView.java
@@ -0,0 +1,17 @@
+package view;
+
+public class ClientView {
+
+    public void displayMenu() {
+        System.out.println("=== Client Menu ===");
+        System.out.println("1. View Auctions");
+        System.out.println("2. View Art Objects");
+        System.out.println("3. Request Service");
+        System.out.println("0. Logout");
+        System.out.print("Select an option: ");
+    }
+    
+    public void showSuccessMessage(String message) {
+        System.out.println("[Client] " + message);
+    }
+}
